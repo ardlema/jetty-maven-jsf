b7593145ec52cecc13d306aa59b1203f113dd1cd
Alberto Rodriguez <alberto.rodriguez@springer.com>
2013-06-15 10:07:49 +0100

    Initial commit

---
+++ src/main/java/mbeans/Visitor.java
@@ -0,0 +1,20 @@
+package org.ardlema.cookbook.mbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class Visitor implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
